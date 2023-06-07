0c36b34c44753f77d22ec58f8010c5ce69e62cb9
Yuliy Daniel <yulik.daniel@gmail.com>
2023-06-07 15:08:06 +0300

    update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for animal in c.elegans ciliate drosophila e.coli human methanocaldococcus mouse thermococcus tuberculosis yeast zebrafish
 do
-   for protein in al_H2A al_H2B al_H3 al_H4 BRD4
+   for protein in H2A H2B H3 H4 BRD4
    do
    	echo "$animal $protein";
-   	~/bin/usr/bin/blastp -query hw2_data/$protein.fasta -db hw2_data/proteomes/$animal.faa -out hw2_data/results/$protein-$animal.blast -outfmt 7;
+   	~/bin/usr/bin/blastp -query hw2_data/$protein.fasta -db hw2_data/proteomes/$animal.faa -out hw2_data/results/$protein-$animal.blast -outfmt 7 -num_threads 16;
    done
 done
